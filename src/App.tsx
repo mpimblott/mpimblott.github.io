5e5697c8a0644a758ca6be4f2cf34ad39f9bae44
Matthew Pimblott <matthew@pimblott.com>
2025-07-08 14:31:39 +0100

    Fix tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 function App() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold mb-4">Matt</h1>
-      <div className="flex flex-row gap-x-80">
-        <a href="#" className="hover:text-blue-300"><FaGithub size={20} /></a>
-        <a href="#" className="hover:text-blue-300"><FaLinkedin size={20} /></a>
+      <h1 className="text-xl font-bold mb-4">Matthew Pimblott</h1>
+      <div className="flex gap-x-4">
+        <a href="https://github.com/mpimblott" className="hover:text-blue-300"><FaGithub size={30} /></a>
+        <a href="https://www.linkedin.com/in/matthew-pimblott/" className="hover:text-blue-300"><FaLinkedin size={30} /></a>
       </div>
     </div>
   );
