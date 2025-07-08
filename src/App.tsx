0875affa50e1365a4ce0afb9b2b6f74f37303c07
Matthew Pimblott <matthew@pimblott.com>
2025-07-08 13:58:07 +0100

    Enable typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-5xl font-bold mb-4">Matt</h1>
+      <div className="flex space-x-4">
+        <a href="#" className="text-blue-400 hover:text-blue-300">GitHub</a>
+        <a href="#" className="text-blue-400 hover:text-blue-300">Twitter</a>
+        <a href="#" className="text-blue-400 hover:text-blue-300">LinkedIn</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
