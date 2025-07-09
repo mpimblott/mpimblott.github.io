ae1ee49f382d651303a89a12af91555fa0d942fd
Matthew Pimblott <matthew@pimblott.com>
2025-07-09 11:35:17 +0100

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,25 @@ import Gridimp from "./pages/gridimp";
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-      <ScrollTracker />
+      <ScrollTracker/>
       <section className="flex flex-col items-center justify-center min-h-screen relative">
         <h1 className="text-6xl font-bold mb-4 text-center">Matthew Pimblott</h1>
         <div className="flex gap-x-4 mb-6">
-          <a href="https://github.com/mpimblott" className="hover:text-blue-300"><FaGithub size={30} /></a>
-          <a href="https://www.linkedin.com/in/matthew-pimblott/" className="hover:text-blue-300"><FaLinkedin size={30} /></a>
+          <a href="https://github.com/mpimblott" className="hover:text-blue-300"><FaGithub size={30}/></a>
+          <a href="https://www.linkedin.com/in/matthew-pimblott/" className="hover:text-blue-300"><FaLinkedin
+            size={30}/></a>
         </div>
-        <SkillsCarousel items={['Machine Learning','Software Development', 'Deep Learning', 'Computer Vision']} />
+        <SkillsCarousel items={['Machine Learning', 'Software Development', 'Deep Learning', 'Computer Vision']}/>
         <div className="absolute bottom-8 left-1/2 -translate-x-1/2">
-          <FaChevronDown size={36} className="animate-bounce text-blue-300" />
+          <FaChevronDown size={36} className="animate-bounce text-blue-300"/>
         </div>
       </section>
-      <main className="flex flex-col items-center w-full">
-        <Gridimp />
+      <main className="flex flex-col items-center w-full px-4 mb-12">
+        <Gridimp/>
       </main>
+      <footer className="w-full py-6 mt-auto text-center text-gray-400">
+        <p>&copy; {new Date().getFullYear()} Matthew Pimblott. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
