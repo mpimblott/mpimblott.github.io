fd379238d6cc92424857633fe39d5b09059c5093
Matthew Pimblott <matthew@pimblott.com>
2025-07-09 11:18:06 +0100

    Adjust tooltip position

--- src/components/bar_chart.tsx
+++ src/components/bar_chart.tsx
@@ -14,21 +14,22 @@ interface BarChartValue {
 function BarChart({values}: { values: BarChartValue[] }) {
   return (<div className="flex w-fit p-6 justify-between gap-1 items-end h-full">
     {values.map((bar, index) => (
-      <div key={index} className="flex relative group h-full items-end">
+      <div key={index} className="flex relative group h-full items-end" role="graphics-symbol"
+           aria-label={`${bar.label}: ${bar.value * 100}%`}>
         <motion.div style={{backgroundColor: bar.colour}}
                     className="w-5 rounded-sm"
                     initial={{height: 0}}
                     whileInView={{height: `${bar.value * 100}%`}}
-                    transition={{duration: 0.4, delay: index * 0.1}}/>
+                    transition={{duration: 0.4, delay: Math.min(index * 0.1, 0.5)}}/>
         <div></div>
-        <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block">
+        <div className="absolute bottom-full left-1/2 -translate-x-1/2 hidden group-hover:block z-10"
+             style={{marginBottom: `${(1-bar.value) * -100}%`}}>
           <div className="bg-gray-800 text-white text-sm px-2 py-2 rounded whitespace-nowrap">
             {bar.label}
           </div>
           <div className="w-2 h-2 bg-gray-800 rotate-45 absolute left-1/2 -translate-x-1/2 -bottom-1"></div>
         </div>
-      </div>
-    ))}
+      </div>))}
   </div>)
 }
 
