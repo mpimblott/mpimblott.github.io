ea17418896b17eda0bac25088326a6672e8528cb
Matthew Pimblott <matthew@pimblott.com>
2025-07-09 14:35:53 +0100

    Improve bar chart positioning and sizing

--- src/components/bar_chart.tsx
+++ src/components/bar_chart.tsx
@@ -12,24 +12,26 @@ interface BarChartValue {
  * @param values The height of the bars relative to the container
  */
 function BarChart({values}: { values: BarChartValue[] }) {
-  return (<div className="flex w-fit p-6 justify-between gap-1 items-end h-full">
-    {values.map((bar, index) => (
-      <div key={index} className="flex relative group h-full items-end" role="graphics-symbol"
-           aria-label={`${bar.label}: ${bar.value * 100}%`}>
-        <motion.div style={{backgroundColor: bar.colour}}
-                    className="w-5 rounded-sm"
-                    initial={{height: 0}}
-                    whileInView={{height: `${bar.value * 100}%`}}
-                    transition={{duration: 0.4, delay: Math.min(index * 0.1, 0.5)}}/>
-        <div></div>
-        <div className="absolute bottom-full left-1/2 -translate-x-1/2 hidden group-hover:block z-10"
-             style={{marginBottom: `${(1-bar.value) * -100}%`}}>
-          <div className="bg-gray-800 text-white text-sm px-2 py-2 rounded whitespace-nowrap">
-            {bar.label}
+  return (<div className="flex justify-center h-full">
+    <div className="flex w-fit p-2 justify-between gap-1 items-end h-full">
+      {values.map((bar, index) => (
+        <div key={index} className="flex relative group h-full items-end" role="graphics-symbol"
+             aria-label={`${bar.label}: ${bar.value * 100}%`}>
+          <motion.div style={{backgroundColor: bar.colour}}
+                      className="w-5 rounded-sm"
+                      initial={{height: 0}}
+                      whileInView={{height: `${bar.value * 100}%`}}
+                      transition={{duration: 0.4, delay: Math.min(index * 0.1, 0.5)}}/>
+          <div></div>
+          <div className="absolute bottom-full left-1/2 -translate-x-1/2 hidden group-hover:block z-10"
+               style={{marginBottom: `${(1.2 - bar.value) * -100}%`}}>
+            <div className="bg-gray-800 text-white text-sm px-2 py-2 rounded whitespace-nowrap">
+              {bar.label}
+            </div>
+            <div className="w-2 h-2 bg-gray-800 rotate-45 absolute left-1/2 -translate-x-1/2 -bottom-1"></div>
           </div>
-          <div className="w-2 h-2 bg-gray-800 rotate-45 absolute left-1/2 -translate-x-1/2 -bottom-1"></div>
-        </div>
-      </div>))}
+        </div>))}
+    </div>
   </div>)
 }
 
